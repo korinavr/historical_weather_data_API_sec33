e7c693aa254c54ac14f53eb94b25427326451d6a
korin <korinabrachnou@gmail.com>
2023-12-23 21:16:43 +0200

    Sec 35: Add URL endpoint for yearly and all data

--- main.py
+++ main.py
@@ -17,18 +17,33 @@ def home():
 
 @app.route('/api/v1/<station>/<date>')
 def about(station, date):
-    try:
-        filename = 'data_small/TG_STAID' + str(station).zfill(6) + '.txt'
-        df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
-        df.columns = [column.strip() for column in df.columns]
-        df['tg0'] = df['TG'].mask(df['TG']==-9999, np.nan)/10
-        temperature = df.loc[df['DATE']==date]['tg0'].squeeze()
-        result_dictionary = {'station': station,
-                             'date': date,
-                             'temperature': temperature}
-        return result_dictionary
-    except FileNotFoundError:
-        return 'This station does not exist'
+    filename = 'data_small/TG_STAID' + str(station).zfill(6) + '.txt'
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    df.columns = [column.strip() for column in df.columns]
+    df['tg0'] = df['TG'].mask(df['TG']==-9999, np.nan)/10
+    temperature = df.loc[df['DATE']==date]['tg0'].squeeze()
+    result_dictionary = {'station': station,
+                         'date': date,
+                         'temperature': temperature}
+    return result_dictionary
+
+
+@app.route('/api/v1/<station>')
+def all_data(station):
+    filename = 'data_small/TG_STAID' + str(station).zfill(6) + '.txt'
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    result = df.to_dict(orient='records')
+    return result
+
+
+@app.route('/api/v1/yearly/<station>/<year>')
+def yearly(station, year):
+    filename = 'data_small/TG_STAID' + str(station).zfill(6) + '.txt'
+    df = pd.read_csv(filename, skiprows=20)
+    df.columns = [column.strip() for column in df.columns]
+    df['DATE'] = df['DATE'].astype(str)
+    result = df[df['DATE'].str.startswith(str(year))].to_dict(orient='records')
+    return result
 
 
 if __name__ == '__main__':
